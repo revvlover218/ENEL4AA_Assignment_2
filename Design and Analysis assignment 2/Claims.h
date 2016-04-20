9383186d5394631b74f7624e7fcc4e8492f2fa7d
revvlover218 <reshalluchman@yahoo.com>
2016-04-20 21:28:51 +0200

    added some vars

--- Design and Analysis assignment 2/Claims.h
+++ Design and Analysis assignment 2/Claims.h
@@ -9,12 +9,19 @@ class Claims
 {
 
 private:
+	int N; //number of times a program has to loop
+	string sxi, syi;
+	int xi, yi, xo, yo;
 
 public:
 
 	Claims();
 	~Claims();
 
+	int getSpace(string); //function which searches for a space in a string and returns the postion of it in the string.
+	int convertString(string, int, int); //function which takes in as parameters the string, a start and finish value 
+	//which is used to create a substring of the string. Once a substring has been
+	//created, the function returns a converted integer version of the substring.
 
 };
 
